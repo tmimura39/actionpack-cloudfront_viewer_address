28d945f76c79e4ec673cf5a898274cbc40ea8313
Tomohiko Mimura <mito.5525@gmail.com>
2024-09-29 15:58:04 +0900

    Initial Commit
    ```sh
    bundle gem actionpack-cloudfront_viewer_address --no-exe --no-coc --no-ext --mit --test=rspec --ci=github --linter=rubocop
    ```

---
+++ lib/actionpack/cloudfront_viewer_address.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "cloudfront_viewer_address/version"
+
+module Actionpack
+  module CloudfrontViewerAddress
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/actionpack/cloudfront_viewer_address/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Actionpack
+  module CloudfrontViewerAddress
+    VERSION = "0.1.0"
+  end
+end
